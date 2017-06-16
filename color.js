de4342cf34be13d8475a3b57c941f6986cd42949
Rahul Sonwalkar <rahulsonwalkar23@gmail.com>
2017-06-16 09:05:56 -0500

    Fixed Timestamp

--- color.js
+++ color.js
@@ -7,10 +7,6 @@
 var chalk = require('chalk');   //Required to color console logs
 var dateTime = require('node-datetime');  //Required to generate Time Stamp
 
-//Generate Time Stamp
-var dt = dateTime.create();
-var formatted = dt.format('H:M');
-
 exports.logMessage = function (username, message, type) {
 if(type == "join") {
   return chalk.black.bgWhite.bold('GORDON') + chalk.yellow(' '+ username + ' joined the chat');
@@ -19,9 +15,17 @@ else if(type == "quit") {
   return chalk.black.bgWhite.bold('GORDON') + chalk.yellow(' '+ username + ' quit the chat');
 }
 else if (type == "incoming") {
+  //Generate Time Stamp
+  var dt = dateTime.create();
+  var formatted = dt.format('H:M');
+
   return chalk.white.bgBlue.bold('GORDON') + ' '+ username + ' @ ' + formatted + '=>  ' + message;
 }
 else if (type == "outgoing"){
+  //Generate Time Stamp
+  var dt = dateTime.create();
+  var formatted = dt.format('H:M');
+  
   return chalk.white.bgGreen.bold('GORDON') + ' '+ username + '(you) @ ' + formatted + '=> ' + message;
 }
 else {
